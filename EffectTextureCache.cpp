6c519cab28b9b1cc667c42a543551d93c28afaaa
JackLN <linyn0612@icloud.com>
2017-08-21 01:03:15 +0800

    add support big tex

--- EffectTextureCache.cpp
+++ EffectTextureCache.cpp
@@ -116,3 +116,106 @@ EffectTextureCache * EffectTextureCache::getInstance(void)
      sink->OnPretrentSuccess();
  }
 
+
+ bool BenchmarkEffect::init()
+ {
+	 if (!Scene::init())
+		 return false;
+
+
+	 _layerDis = LayerColor::create(Color4B::BLACK);
+	 addChild(_layerDis);
+
+	 auto winSize = Director::getInstance()->getWinSize();
+	 auto originRect = Director::getInstance()->getVisibleOrigin();
+	 auto visibleSize = Director::getInstance()->getVisibleSize();
+
+	 SpriteFrameCache::getInstance()->addSpriteFramesWithFile("Boilogys1.plist");
+
+	 Sprite* test = Sprite::createWithSpriteFrameName("Biology0_1.png");
+	 test->setPosition(200, 200);
+	 _layerDis->addChild(test);
+
+	 auto frame = SpriteFrameCache::getInstance()->getSpriteFrameByName("Biology0_1.png");
+
+	 auto rect = frame->getRectInPixels();
+	 auto rotate = frame->isRotated();
+	 auto offsetPix = rect.origin;
+
+	 std::string fullpath = FileUtils::getInstance()->fullPathForFilename(frame->getTexture()->getPath());
+	 auto image = new Image();
+	 image->initWithImageFile(fullpath);
+
+	 unsigned char* pImgData = image->getData();
+	 auto iDataLen = image->getDataLen();
+	 int iWidth = frame->getOriginalSizeInPixels().width;
+	 int iHeight = frame->getOriginalSizeInPixels().height;
+
+	 /*int iWidth = rect.size.width;
+	 int iHeight = rect.size.height;*/
+	 int iOriginWidth = image->getWidth();
+	 int iOriginHeight = image->getHeight();
+
+
+
+	 //target data
+	 unsigned char* pTarData = (unsigned char*)(malloc(iWidth * iHeight * 4 * sizeof(unsigned char)));
+	 memset(pTarData, 0, iWidth * iHeight * 4 * sizeof(unsigned char));
+	 //change image buffer data
+	 int i;
+	 int j;
+	 for (i = 0; i < iWidth; ++i)
+	 {
+		 for (j = 0; j < iHeight; ++j)
+		 {
+			 int offset = (iWidth* i + j) * 4;
+			 //int originOffset = (iOriginWidth * (i) + j ) * 4;
+			 int originOffset = (iOriginWidth * (i + (int)offsetPix.y) + j + (int)offsetPix.x) * 4;
+			 *(pTarData + offset + 0) = *(pImgData + originOffset + 0);
+			 *(pTarData + offset + 1) = *(pImgData + originOffset + 1);
+			 *(pTarData + offset + 2) = *(pImgData + originOffset + 2);
+			 *(pTarData + offset + 3) = *(pImgData + originOffset + 3);
+
+			 //int originOffset = ((iWidth + origin.width) * i + j + origin.height) * 4;
+			 /**(pTarData + offset + 0) = *(pImgData + originOffset + 0);
+			 *(pTarData + offset + 1) = *(pImgData + originOffset + 1);
+			 *(pTarData + offset + 2) = *(pImgData + originOffset + 2);
+			 *(pTarData + offset + 3) = *(pImgData + originOffset + 3);*/
+		 }
+	 }
+
+	 //copy image data
+	 //memcpy(image->getData(), pTarData, image->getDataLen());
+	 //CC_SAFE_FREE(pTarData);
+
+	 //create texture & sprite
+	 Texture2D* tex = new Texture2D();
+	 tex->initWithData(pTarData, iWidth * iHeight * 4, Texture2D::PixelFormat::RGBA8888, iWidth, iHeight, CCSize(iWidth, iHeight));
+	 //EffectTextureCache::getInstance()->addTextureWithName(filename, _effectName, tex);
+	 CC_SAFE_DELETE(image);
+
+	 Sprite* demo = Sprite::createWithTexture(tex);
+	 demo->setPosition(200, 400);
+	 //demo->setScale(0.8);
+	 _layerDis->addChild(demo);
+
+
+	 /*Rect origin = pFoo->getBoundingBox();
+	 auto pRender = RenderTexture::create(origin.size.width, origin.size.height, kCCTexture2DPixelFormat_RGBA8888);
+	 pRender->beginWithClear(0.0f, 0.0f, 0.0f, 0.0f);
+	 auto tmpSp = Sprite::createWithSpriteFrame(pFoo->getSpriteFrame());
+	 tmpSp->setPosition(tmpSp->getContentSize() / 2);
+	 pGlow->setTarget(tmpSp);
+	 tmpSp->visit();
+	 pRender->end();
+	 pRender->saveToFile("Outer.png", cocos2d::Image::Format::PNG);*/
+
+	 /* Rect origin = pFoo->getBoundingBox();
+	 auto pRender = RenderTexture::create(winSize.width, winSize.height, kCCTexture2DPixelFormat_RGBA8888);
+	 pRender->beginWithClear(0.0f, 0.0f, 0.0f, 0.0f);
+	 pFoo->visit();
+	 pRender->end();
+	 pRender->saveToFile("Outer.png", cocos2d::Image::Format::PNG);*/
+
+	 return true;
+ }
